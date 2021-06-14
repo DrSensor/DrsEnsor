34314f218784edb16d754d271924c00e530d67b9
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2021-06-15 02:57:21 +0700

    Add sophisticated shellHook
    - always gpgsign all operation
    - set upstream repo if not set
    - add remote "all" for pushing to all repo (origin and upstream)

--- flake.nix
+++ flake.nix
@@ -14,6 +14,26 @@
           ssg = [ zola ];
         in
           ssg ++ langtools;
+        upstreamRepo = "git@git.sr.ht:~drsensor/site";
+        shellHook = ''
+          git config push.gpgsign if-asked
+          git config commit.gpgsign true
+          git config tag.gpgsign true
+          if ! git remote show | grep -q upstream && [ $(git remote get-url origin) != $upstreamRepo ]; then
+            git config checkout.defaultRemote upstream
+            git config remote.pushDefault upstream
+
+            defaultBranch=$(git remote show origin | awk '/HEAD branch/ {print $NF}')
+            git config branch.$defaultBranch.remote upstream
+            git config branch.$defaultBranch.merge refs/heads/$defaultBranch
+
+            git remote add upstream $upstreamRepo
+            git remote add all $(git remote get-url upstream)
+            for remote in origin upstream; do
+              git remote set-url --add --push all $(git remote get-url $remote)
+            done
+          fi
+        '';
       };
     };
 }
